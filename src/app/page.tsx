a6fdff55658e0b7b203496a276a456c75d5aa91d
viniciuspaiva1 <viniciuspaivarpg@gmail.com>
2025-08-06 08:41:53 -0300

    feat: add products list and productItem components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,46 @@
-import Header from "@/components/common/header";
-import { Button } from "@/components/ui/button";
+import Image from "next/image";
 
-const Home = () => {
+import Header from "@/components/common/header";
+import ProductList from "@/components/common/product-list";
+import { Button } from "@/components/ui/button";
+import { db } from "@/db";
+
+const Home = async () => {
+  const products = await db.query.productTable.findMany({
+    with: {
+      variants: true,
+    },
+  });
+  console.log(products);
   return (
     <>
       <Header />
+
+      <div className="space-y-6">
+        <div className="px-5">
+          <Image
+            src="/banner-01.png"
+            alt="Leve uma vida com estilo"
+            width={0}
+            height={0}
+            sizes="100vw"
+            className="h-auto w-full"
+          />
+        </div>
+
+        <ProductList products={products} title="Mais Vendidos" />
+
+        <div className="px-5">
+          <Image
+            src="/banner-02.png"
+            alt="Seja autêntico"
+            width={0}
+            height={0}
+            sizes="100vw"
+            className="h-auto w-full"
+          />
+        </div>
+      </div>
     </>
   );
 };
